f1ef485850ee91b477ca227b9f9476f32c67a42a
Sultan Mahmud <mdsultanmahmud.bd00@gmail.com>
2022-11-08 04:54:12 +0600

    add home page banner

--- src/pages/home/Banner/Banner.jsx
+++ src/pages/home/Banner/Banner.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
-
+import banner from '../../../assets/banner.jpg'
 const Banner = () => {
     return (
-        <div>
-             <h3>Banner</h3>
-        </div>
+            <div className="hero min-h-screen" style={{ backgroundImage: `url("${banner}")`, backgroundAttachment:'fixed' }}>
+                <div className="hero-overlay bg-opacity-70"></div>
+                <div className="hero-content text-center text-neutral-content">
+                    <div className="max-w-md">
+                        <h1 className="mb-5 text-4xl font-bold">Made by Me at Home</h1>
+                        <p className="mb-5">I serve fresh and testy food. Delivery fast and discount all time. Order from me, cause it's purely made by me, all time neat and clean</p>
+                        <button className="btn btn-warning btn-outline">Explore our food</button>
+                    </div>
+                </div>
+            </div>
     );
 };
 
